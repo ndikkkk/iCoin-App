52e2044005aea794b2c818e7225557422c72427a
Andhikaalvn <andhi618@gmail.com>
2026-01-13 11:43:37 +0700

    Update gitignore and secure keys

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,24 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Ganti string keras dengan import.meta.env
 const firebaseConfig = {
-  apiKey: "AIzaSyDjq6kQNuCmkcm1IGUhWkgpEZUhV4T5jZU",
-  authDomain: "icoin-app-c11eb.firebaseapp.com",
-  projectId: "icoin-app-c11eb",
-  storageBucket: "icoin-app-c11eb.firebasestorage.app",
-  messagingSenderId: "774536860312",
-  appId: "1:774536860312:web:7664ab8bfce801c43df6ed"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export service yang akan kita pakai
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 export const db = getFirestore(app);
